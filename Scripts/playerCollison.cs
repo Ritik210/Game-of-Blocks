dcb7c44ef8490085574253228b673040ec811638
Ritik210 <hritikagarwal50@gmail.com>
2020-08-13 20:20:21 +0530

    Col

---
+++ Scripts/playerCollison.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class playerCollison : MonoBehaviour
+{
+    public PlayersMovement movement; 
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        
+        //Debug.Log(collisionInfo.collider.name);
+        if(collisionInfo.collider.tag == "Collidor")
+        {
+            movement.enabled = false;
+            FindObjectOfType<GameManager>().EndGame();
+            
+        }
+    }
+}
