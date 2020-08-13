70f8afc8a7433e2bbdcfe181e4aacaf71eb22b49
Ritik210 <hritikagarwal50@gmail.com>
2020-08-13 20:22:37 +0530

    Sc

---
+++ Scripts/Score.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public Transform player;
+    public Text scoreText;
+    public float playerPosition = 0f;
+
+    // Update is called once per frame
+    private void Update() => scoreText.text = playerPosition + player.position.z.ToString();// Debug.Log(player.position.z);
+}
